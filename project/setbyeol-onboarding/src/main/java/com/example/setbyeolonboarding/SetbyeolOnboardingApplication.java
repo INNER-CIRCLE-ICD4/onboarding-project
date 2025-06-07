aa7699511b1e3dbf70c4931ce798a4deea18b512
SETBYEOL\setby <itcrowd91@gmail.com>
2025-06-07 18:06:36 +0900

    init: Spring Boot 세팅

---
+++ project/setbyeol-onboarding/src/main/java/com/example/setbyeolonboarding/SetbyeolOnboardingApplication.java
@@ -0,0 +1,15 @@
+package com.example.setbyeolonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetbyeolOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetbyeolOnboardingApplication.class, args);
+        System.out.println("hello word");
+    }
+
+
+}
