da3ef1fcc9248085ccf9ec3227b83c081d8efe3e
tommyheo <jyhuh8775@naver.com>
2025-06-07 17:59:16 +0900

    프로젝트 디렉토리 생성

---
+++ project/jaeyoung-onboarding/src/test/java/com/tommy/jaeyoungonboarding/JaeyoungOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.jaeyoungonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaeyoungOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
