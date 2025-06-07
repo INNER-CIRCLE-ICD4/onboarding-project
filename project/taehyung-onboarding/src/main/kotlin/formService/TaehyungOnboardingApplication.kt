3473e9cf70d80b10434f46a3e540189806333759
autumn-sun91 <dpwe231@gmail.com>
2025-06-07 17:36:04 +0900

    [feat] init project

---
+++ project/taehyung-onboarding/src/main/kotlin/formService/TaehyungOnboardingApplication.kt
@@ -0,0 +1,11 @@
+package formService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaehyungOnboardingApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaehyungOnboardingApplication>(*args)
+}
