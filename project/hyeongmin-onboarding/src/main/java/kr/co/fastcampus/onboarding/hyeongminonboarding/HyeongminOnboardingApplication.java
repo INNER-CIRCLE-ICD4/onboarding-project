9ab6d913b51758ace6635daff89e824f833f4a51
commin-pg <khm.commin@gmail.com>
2025-06-07 17:41:49 +0900

    source init  - JDK 17  - JPA (H2)  - Gradle

---
+++ project/hyeongmin-onboarding/src/main/java/kr/co/fastcampus/onboarding/hyeongminonboarding/HyeongminOnboardingApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.onboarding.hyeongminonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeongminOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyeongminOnboardingApplication.class, args);
+    }
+
+}
