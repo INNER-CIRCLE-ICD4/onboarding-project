ee510d54dc0ab57d67d7a967346dd920498dcf94
tommyheo <jyhuh8775@naver.com>
2025-06-08 10:47:39 +0900

    [2025-06-08 : CREATE] ResourceNotFoundException 생성

--- jaeyoung-onboarding/src/main/java/com/tommy/jaeyoungonboarding/service/SurveyService.java
+++ jaeyoung-onboarding/src/main/java/com/tommy/jaeyoungonboarding/service/SurveyService.java
@@ -10,5 +10,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SurveyService {
 
+    /*
+    * 모든 Survey 조회
+    * @return Survey 리스트
+    * */
+    public List<Survey> selectAllSurvey(){
 
+        List<Survey> selectAllSurvey = surveyRepository.selectAllSurvey();
+
+        if(selectAllSurvey.isEmpty()){
+
+        }
+
+        return null;
+    }
 }
