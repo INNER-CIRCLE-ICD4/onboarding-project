a041c1532b9ce48e041e450454add3a6df7fd357
changzunelee <changzunelee@gmail.com>
2025-06-09 21:10:28 +0900

    spring boot setting

---
+++ project/changzune-onboarding/src/main/java/com/innercircle/onboarding/changzune_onboarding/ChangzuneOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.innercircle.onboarding.changzune_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangzuneOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangzuneOnboardingApplication.class, args);
+	}
+
+}
