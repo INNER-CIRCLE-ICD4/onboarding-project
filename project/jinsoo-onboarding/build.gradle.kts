5fb03d1496ce060fd8043dea64c1c0bdae18bda5
ckaanf <skywlstn777@gmail.com>
2025-06-09 20:01:25 +0900

    chore: #10 - 멀티모듈 구성에 따른 .kts 변경

--- project/jinsoo-onboarding/build.gradle.kts
+++ project/jinsoo-onboarding/build.gradle.kts
@@ -7,31 +7,36 @@ plugins {
 group = "com.icd"
 version = "0.0.1-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
     }
 }
 
-repositories {
-    mavenCentral()
-}
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    runtimeOnly("com.h2database:h2")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+
+    dependencies {
+        "compileOnly"("org.projectlombok:lombok")
+        "annotationProcessor"("org.projectlombok:lombok")
+
+        "testImplementation"("org.springframework.boot:spring-boot-starter-test")
+        "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
--- project/jinsoo-onboarding/settings.gradle.kts
+++ project/jinsoo-onboarding/settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "jinsoo-onboarding"
+
+include("common")
+include("service")
+include("service:survey")
+include("service:survey-read")
