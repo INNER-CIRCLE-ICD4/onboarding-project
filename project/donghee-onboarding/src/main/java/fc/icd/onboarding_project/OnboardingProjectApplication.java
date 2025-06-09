cc88c840aaa35ecb318e0ddcfa67aa90627233ea
donghee <weg9511@gmail.com>
2025-06-09 21:06:38 +0900

    [개인 디렉토리 생성] 개인 디렉토리 생성 및 신규 SpringBoot 프로젝트 생성

---
+++ project/donghee-onboarding/src/main/java/fc/icd/onboarding_project/OnboardingProjectApplication.java
@@ -0,0 +1,13 @@
+package fc.icd.onboarding_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingProjectApplication.class, args);
+	}
+
+}
---
+++ project/donghee-onboarding/src/test/java/fc/icd/onboarding_project/OnboardingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fc.icd.onboarding_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
