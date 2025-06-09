64257e1808da7597ce22afc739adb14f8758d81a
jojunbeom <jb.cho@okestro.com>
2025-06-09 14:14:15 +0900

    init

---
+++ project/junbeom-onboarding/src/main/java/fastcampus/junbeom_onboarding/JunbeomOnboardingApplication.java
@@ -0,0 +1,13 @@
+package fastcampus.junbeom_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunbeomOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunbeomOnboardingApplication.class, args);
+	}
+
+}
---
+++ project/junbeom-onboarding/src/test/java/fastcampus/junbeom_onboarding/JunbeomOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.junbeom_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunbeomOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
