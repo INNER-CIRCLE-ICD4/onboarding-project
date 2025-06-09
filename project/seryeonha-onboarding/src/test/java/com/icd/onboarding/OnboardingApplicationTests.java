f8ef3b784060bba5dedddea69a4c406b02354ad9
pathfinder13hsr <pathfinder13hsr@gmail.com>
2025-06-09 17:32:51 +0900

    패키지 네이밍 변경

--- project/seryeonha-onboarding/src/test/java/com/icd/onboarding/OnboardingApplicationTests.java
+++ project/seryeonha-onboarding/src/test/java/com/icd/onboarding/OnboardingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.pathfinder.survey;
+package com.icd.onboarding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SurveyApplicationTests {
+class OnboardingApplicationTests {
 
     @Test
     void contextLoads() {
