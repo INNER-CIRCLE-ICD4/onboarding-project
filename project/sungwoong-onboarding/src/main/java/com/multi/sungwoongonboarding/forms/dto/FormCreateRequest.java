368515f63d447603860b4eb48c068ba7b2caf4e0
sungwoongNoh <asdz453@gmail.com>
2025-06-09 20:53:51 +0900

    feat(form): 설문지 등록 요청 객체에 validator 적용 및 테스트

--- project/sungwoong-onboarding/src/main/java/com/multi/sungwoongonboarding/forms/dto/FormCreateRequest.java
+++ project/sungwoong-onboarding/src/main/java/com/multi/sungwoongonboarding/forms/dto/FormCreateRequest.java
@@ -1,6 +1,10 @@
 package com.multi.sungwoongonboarding.forms.dto;
 
 import com.multi.sungwoongonboarding.questions.dto.QuestionCreateRequest;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +15,14 @@ import java.util.List;
 @Builder
 @RequiredArgsConstructor
 public class FormCreateRequest {
+
+    @NotBlank(message = "제목은 필수 입력 항목입니다.")
     private final String title;
+
     private final String description;
+
+    @Min(value = 1, message = "최소 1개 이상의 질문이 필요합니다.")
+    @Max(value = 10, message = "최대 10개 이하의 질문을 작성할 수 있습니다.")
     private final List<QuestionCreateRequest> questionCreateRequests;
 
 }
