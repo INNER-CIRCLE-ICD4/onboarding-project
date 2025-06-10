ff6da0d0c8667f48973acb8641f8ff4e3800511f
sungwoongNoh <asdz453@gmail.com>
2025-06-10 19:49:10 +0900

    fix(common): enum validator 수정 - 공통으로 사용될 수 있도록 수정

--- project/sungwoong-onboarding/src/main/java/com/multi/sungwoongonboarding/common/valid/ValidEnum.java
+++ project/sungwoong-onboarding/src/main/java/com/multi/sungwoongonboarding/common/valid/ValidEnum.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = QuestionEnumValidator.class)
+@Constraint(validatedBy = EnumValidator.class)
 @Target({ElementType.FIELD, ElementType.TYPE_PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEnum {
