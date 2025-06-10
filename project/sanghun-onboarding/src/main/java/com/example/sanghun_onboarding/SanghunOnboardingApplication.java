eb60f260f50681f362d60ac962004136c5572857
Pakgitt <158733754+Pakgitt@users.noreply.github.com>
2025-06-10 20:24:12 +0900

    psh:개인디렉토리 생성 및 Spring Boot 프로젝트 생성

---
+++ project/sanghun-onboarding/src/main/java/com/example/sanghun_onboarding/SanghunOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanghun_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanghunOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanghunOnboardingApplication.class, args);
+	}
+
+}
