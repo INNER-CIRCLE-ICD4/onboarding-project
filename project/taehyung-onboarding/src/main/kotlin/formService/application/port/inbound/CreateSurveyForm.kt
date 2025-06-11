37cec5e036fe3d4348d3b6ec09df72c92f8296ad
autumn-sun91 <dpwe231@gmail.com>
2025-06-11 20:25:10 +0900

    [feat] 설문지 생성 유스케이스 구현(1)

--- project/taehyung-onboarding/src/main/kotlin/formService/application/port/inbound/CreateSurveyForm.kt
+++ project/taehyung-onboarding/src/main/kotlin/formService/application/port/inbound/CreateSurveyForm.kt
@@ -1,5 +1,25 @@
 package formService.application.port.inbound
 
+import formService.domain.Question
+
 interface CreateSurveyForm {
-    fun createSurveyForm()
+    fun createSurveyForm(command: CreateSurveyFormCommand)
+
+    data class CreateSurveyFormCommand(
+        var surveyName: String,
+        var description: String,
+        val questions: List<CreateSurveyFormQuestion>,
+    )
+
+    data class CreateSurveyFormQuestion(
+        var name: String,
+        var description: String,
+        var inputType: Question.QuestionInputType,
+        var required: Boolean,
+        val options: List<CreateSurveyFormQuestionOption>?,
+    )
+
+    data class CreateSurveyFormQuestionOption(
+        var value: String,
+    )
 }
