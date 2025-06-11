775bca4d75a99340671872c0b0db596dd0dc929e
kimmang94 <kimmang940715@naver.com>
2025-06-12 00:36:09 +0900

    Entity 구현

---
+++ project/hyeok-onboarding/src/main/java/com/INNER_CIRCLE_ICD4/innerCircle/domain/AnswerChoice.java
@@ -0,0 +1,33 @@
+package com.INNER_CIRCLE_ICD4.innerCircle.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AnswerChoice {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "choice_id")
+    private Choice choice;
+
+    public AnswerChoice(Choice choice) {
+        this.choice = choice;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+}
