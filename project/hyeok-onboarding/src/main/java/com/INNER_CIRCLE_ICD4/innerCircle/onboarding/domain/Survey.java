4d7298ad939afa77b82a4f8cf3cef9382c9e2f80
kimmang94 <kimmang940715@naver.com>
2025-06-12 00:06:42 +0900

    Entity 추가 - Answer - AnswerChoice - Choice - Question - QuestionType - Response - Survey

---
+++ project/hyeok-onboarding/src/main/java/com/INNER_CIRCLE_ICD4/innerCircle/onboarding/domain/Survey.java
@@ -0,0 +1,50 @@
+package com.INNER_CIRCLE_ICD4.innerCircle.onboarding.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Survey {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+    private String description;
+
+    private int version = 1;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "survey", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderColumn(name = "question_order")
+    private List<Question> questions = new ArrayList<>();
+
+    public Survey(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void update(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.version += 1;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void addQuestion(Question question) {
+        question.setSurvey(this);
+        this.questions.add(question);
+    }
+}
