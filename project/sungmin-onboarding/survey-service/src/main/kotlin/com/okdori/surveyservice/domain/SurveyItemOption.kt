0de840ce43e66ac582c9f1d7d1a632aceda7a169
m1ngoDo <vsm0803@gmail.com>
2025-06-11 22:53:53 +0900

    도메인 설계 및 ERD 작성

---
+++ project/sungmin-onboarding/survey-service/src/main/kotlin/com/okdori/surveyservice/domain/SurveyItemOption.kt
@@ -0,0 +1,40 @@
+package com.okdori.surveyservice.domain
+
+import com.okdori.surveyservice.domain.base.BaseTimeEntity
+import io.hypersistence.utils.hibernate.id.Tsid
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+/**
+ * packageName    : com.okdori.surveyservice.domain
+ * fileName       : SurveyItemOption
+ * author         : okdori
+ * date           : 2025. 6. 11.
+ * description    :
+ */
+
+@Table(name = "t_survey_item_option")
+@Entity
+class SurveyItemOption(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    var surveyItem: SurveyItem
+): BaseTimeEntity() {
+    @Id @Tsid
+    @Column(name = "option_id", columnDefinition = "CHAR(13)")
+    var id: String? = null
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(200)")
+    var optionName = ""
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN")
+    var isOtherOption = false
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN")
+    var isDeleted = false
+}
---
+++ project/sungmin-onboarding/survey-service/src/main/kotlin/com/okdori/surveyservice/domain/SurveyResponseAnswer.kt
@@ -0,0 +1,48 @@
+package com.okdori.surveyservice.domain
+
+import com.okdori.surveyservice.domain.base.BaseTimeEntity
+import io.hypersistence.utils.hibernate.id.Tsid
+import io.hypersistence.utils.hibernate.type.json.JsonType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.hibernate.annotations.Type
+
+/**
+ * packageName    : com.okdori.surveyservice.domain
+ * fileName       : SurveyResponseAnswer
+ * author         : okdori
+ * date           : 2025. 6. 11.
+ * description    :
+ */
+
+@Table(name = "t_survey_response_answer")
+@Entity
+class SurveyResponseAnswer(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "response_id")
+    var surveyResponse: SurveyResponse,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    var surveyItem: SurveyItem
+): BaseTimeEntity() {
+    @Id @Tsid
+    @Column(name = "answer_id", columnDefinition = "CHAR(13)")
+    var id: String? = null
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(200)")
+    var itemName = ""
+
+    @Type(JsonType::class)
+    @Column(columnDefinition = "JSON")
+    var answer: Answer? = null
+}
+
+data class Answer(
+    val option: String,
+)
