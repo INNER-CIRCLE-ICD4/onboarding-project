199a8a64083ba62878707fc4ef9b83d96f541706
HwangInUng <ung6860@naver.com>
2025-06-11 19:41:22 +0900

    feat: Service 레이어 테스트 클래스 생성

---
+++ project/inung-onboarding/src/main/java/icd/onboarding/surveyproject/service/SurvayService.java
@@ -0,0 +1,4 @@
+package icd.onboarding.surveyproject.service;
+
+public class SurvayService {
+}
---
+++ project/inung-onboarding/src/test/java/icd/onboarding/surveyproject/service/SurvayServiceTest.java
@@ -0,0 +1,13 @@
+package icd.onboarding.surveyproject.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SurvayServiceTest {
+
+	@Test
+	@DisplayName("테스트 클래스 세팅")
+	void test1() {
+
+	}
+}
