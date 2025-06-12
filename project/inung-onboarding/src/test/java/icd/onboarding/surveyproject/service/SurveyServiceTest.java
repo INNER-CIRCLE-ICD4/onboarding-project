cdb6f20091eb3168eca4759c13cb8588f3d1def6
HwangInUng <ung6860@naver.com>
2025-06-12 20:07:24 +0900

    fix: 코드 충돌 해결-3

--- project/inung-onboarding/src/test/java/icd/onboarding/surveyproject/service/SurveyServiceTest.java
+++ project/inung-onboarding/src/test/java/icd/onboarding/surveyproject/service/SurveyServiceTest.java
@@ -1,6 +1,5 @@
 package icd.onboarding.surveyproject.service;
 
-import icd.onboarding.surveyproject.common.NotImplementedTestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,19 +12,18 @@ class SurveyServiceTest {
 		@Test
 		@DisplayName("설문 조사의 이름, 설명, 질문의 값이 없는 경우 IllegalArgumentException을 반환 한다.")
 		void test1 () {
-			throw new NotImplementedTestException();
+
 		}
 
 		@Test
 		@DisplayName("질문의 수가 없는 경우 InSufficientQuestionsException을 반환 한다.")
 		void test2 () {
-			throw new NotImplementedTestException();
 		}
 
 		@Test
 		@DisplayName("질문의 수가 10개를 초과한 경우 MaxQuestionLimitExceededException을 반환 한다.")
 		void test3 () {
-			throw new NotImplementedTestException();
+
 		}
 	}
 }
