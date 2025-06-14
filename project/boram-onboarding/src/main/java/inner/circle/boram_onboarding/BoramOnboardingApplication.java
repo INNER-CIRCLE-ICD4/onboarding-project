1f88c4a5b86634516672a4901411177bd24ff030
boram0415 <boram04415@naver.com>
2025-06-14 11:01:22 +0900

    set up

---
+++ project/boram-onboarding/src/main/java/inner/circle/boram_onboarding/BoramOnboardingApplication.java
@@ -0,0 +1,14 @@
+package inner.circle.boram_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoramOnboardingApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(BoramOnboardingApplication.class, args);
+	}
+
+}
---
+++ project/boram-onboarding/src/test/java/inner/circle/boram_onboarding/BoramOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package inner.circle.boram_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoramOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
