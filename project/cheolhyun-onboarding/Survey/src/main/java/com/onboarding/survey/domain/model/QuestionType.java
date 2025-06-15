8d6ecdd27097ef46665b47a74b3fea0fb78ecb97
songsimo <chy.song89@gmail.com>
2025-06-15 16:05:01 +0900

    fix: 멀티 모듈 초기 구성

--- project/cheolhyun-onboarding/Survey/src/main/java/com/onboarding/survey/domain/model/QuestionType.java
+++
@@ -1,8 +0,0 @@
-package com.onboarding.survey.domain.model;
-
-public enum QuestionType {
-    SHORT_TEXT,
-    LONG_TEXT,
-    SINGLE_CHOICE,
-    MULTI_CHOICE
-}
---
+++ project/cheolhyun-onboarding/core/src/test/java/survey/domain/model/SurveyTest.java
@@ -0,0 +1,19 @@
+package survey.domain.model;
+
+import com.onboarding.model.Survey;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SurveyTest {
+    @Test
+    @DisplayName("null과 빈값 유효성 검증")
+    void validEmpty() {
+        assertAll(
+                () -> assertThatThrownBy(() -> new Survey("")).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> new Survey(null)).isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+}
