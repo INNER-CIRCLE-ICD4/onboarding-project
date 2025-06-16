82c622e57e8aadd8e4973eee1382f9a0ce1ca7d9
ckaanf <skywlstn777@gmail.com>
2025-06-16 21:34:24 +0900

    fix: #10 - request 객체 변경

--- project/jinsoo-onboarding/service/survey/src/main/java/survey/survey/controller/request/SurveyFormCreateRequest.java
+++ project/jinsoo-onboarding/service/survey/src/main/java/survey/survey/controller/request/SurveyFormCreateRequest.java
@@ -4,30 +4,16 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import survey.survey.controller.validator.ValidQuestion;
 import survey.survey.entity.surveyquestion.InputType;
 
 import java.util.List;
 
-@Getter
-public class SurveyFormCreateRequest {
-    private Long surveyId;
-
-    @NotBlank(message = "설문조사 이름은 필수입니다")
-    private String title;
-
-    @NotBlank(message = "설문조사 설명은 필수입니다")
-    private String description;
-
-    @NotNull(message = "질문 목록은 필수입니다")
-    @Size(min = 1, max = 10, message = "질문은 1개 이상 10개 이하여야 합니다")
-    @Valid
-    private List<QuestionCreateRequest> questionList;
-
-    public SurveyFormCreateRequest() {
-    }
-
+public record SurveyFormCreateRequest(Long surveyId,
+                                      @NotBlank(message = "설문조사 이름은 필수입니다") String title,
+                                      @NotBlank(message = "설문조사 설명은 필수입니다") String description,
+                                      @NotNull(message = "질문 목록은 필수입니다")
+                                      @Size(min = 1, max = 10, message = "질문은 1개 이상 10개 이하여야 합니다")
+                                      @Valid List<QuestionCreateRequest> questionList) {
     public SurveyFormCreateRequest(Long surveyId, String title, String description, List<QuestionCreateRequest> questionList) {
         this.surveyId = surveyId;
         this.title = title;
@@ -35,28 +21,11 @@ public class SurveyFormCreateRequest {
         this.questionList = questionList;
     }
 
-    @ValidQuestion
-    @Getter
-    public static class QuestionCreateRequest {
-        @NotBlank(message = "질문 이름은 필수입니다")
-        private String name;
-
-        private int questionIndex;
-
-        @NotBlank(message = "질문 설명은 필수입니다")
-        private String description;
-
-        @NotNull(message = "입력 형태는 필수입니다")
-        private InputType inputType;
-
-        private boolean required;
-
-        @Valid
-        private List<CandidateCreateRequest> candidates;
-
-        public QuestionCreateRequest() {
-        }
-
+    public record QuestionCreateRequest(
+            @NotBlank(message = "질문 이름은 필수입니다") String name, int questionIndex,
+            @NotBlank(message = "질문 설명은 필수입니다") String description,
+            @NotNull(message = "입력 형태는 필수입니다") InputType inputType, boolean required,
+            @Valid List<CandidateCreateRequest> candidates) {
         public QuestionCreateRequest(String name, int questionIndex, String description,
                                      InputType inputType, boolean required, List<CandidateCreateRequest> candidates) {
             this.name = name;
@@ -67,15 +36,9 @@ public class SurveyFormCreateRequest {
             this.candidates = candidates;
         }
 
-        @Getter
-        public static class CandidateCreateRequest {
-            private int checkCandidateIndex;
-            @NotBlank(message = "선택 항목 값은 필수입니다")
-            private String name;
-
-            public CandidateCreateRequest() {
-            }
-
+        public record CandidateCreateRequest(
+                int checkCandidateIndex,
+                @NotBlank(message = "선택 항목 값은 필수입니다") String name) {
             public CandidateCreateRequest(int checkCandidateIndex, String name) {
                 this.checkCandidateIndex = checkCandidateIndex;
                 this.name = name;
