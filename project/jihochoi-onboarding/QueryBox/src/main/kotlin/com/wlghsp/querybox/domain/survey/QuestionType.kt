92140401cb218f640cbbb6ab49384154140b4e38
JIHO CHOI <jh.choi2@okestro.com>
2025-06-18 10:28:05 +0900

    add: QuestionType Enum 클래스 및 테스트 추가

--- project/jihochoi-onboarding/QueryBox/src/main/kotlin/com/wlghsp/querybox/domain/survey/QuestionType.kt
+++ project/jihochoi-onboarding/QueryBox/src/main/kotlin/com/wlghsp/querybox/domain/survey/QuestionType.kt
@@ -1,5 +1,8 @@
 package com.wlghsp.querybox.domain.survey
 
 enum class QuestionType {
-    SHORT_TEXT, LONG_TEXT, SINGLE_CHOICE, MULTIPLE_CHOICE
+    SHORT_TEXT, LONG_TEXT, SINGLE_CHOICE, MULTIPLE_CHOICE;
+
+    // 단일 or 다중 선택 리스트 인지 확인
+    fun isChoice(): Boolean = this == SINGLE_CHOICE || this == MULTIPLE_CHOICE
 }
