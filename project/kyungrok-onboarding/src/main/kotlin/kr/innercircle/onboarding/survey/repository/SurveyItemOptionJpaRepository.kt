3a90e2d6c83ecbb1b6854897fb455beb1b1c3b3a
ckr <ckr3453@gmail.com>
2025-06-21 01:39:30 +0900

    feat: 설문조사 항목 옵션 생성 기능 및 예외처리 추가

---
+++ project/kyungrok-onboarding/src/main/kotlin/kr/innercircle/onboarding/survey/repository/SurveyItemOptionJpaRepository.kt
@@ -0,0 +1,14 @@
+package kr.innercircle.onboarding.survey.repository
+
+import kr.innercircle.onboarding.survey.domain.SurveyItemOption
+import org.springframework.data.jpa.repository.JpaRepository
+
+/**
+ * packageName : kr.innercircle.onboarding.survey.repository
+ * fileName    : SurveyItemOptionJpaRepository
+ * author      : ckr
+ * date        : 25. 6. 17.
+ * description :
+ */
+interface SurveyItemOptionJpaRepository: JpaRepository<SurveyItemOption, Long> {
+}
