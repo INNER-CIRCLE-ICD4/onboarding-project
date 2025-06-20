86b29382f2df6a57d350ccbde5e0ae25c055a728
ckr <ckr3453@gmail.com>
2025-06-21 05:45:34 +0900

    feat: 설문조사 응답 등록 예외처리 추가

--- project/kyungrok-onboarding/src/main/kotlin/kr/innercircle/onboarding/survey/dto/request/CreateSurveyResponseRequest.kt
+++ project/kyungrok-onboarding/src/main/kotlin/kr/innercircle/onboarding/survey/dto/request/CreateSurveyResponseRequest.kt
@@ -1,8 +1,9 @@
 package kr.innercircle.onboarding.survey.dto.request
 
-import jakarta.validation.constraints.Min
+import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 
 /**
  * packageName : kr.innercircle.onboarding.survey.dto.request
@@ -15,7 +16,8 @@ data class CreateSurveyResponseRequest(
     @field:NotBlank(message = "설문조사 응답자 필수값입니다.")
     val respondent: String,
 
+    @field:Valid
     @field:NotNull(message = "설문조사 답변은 필수값 입니다.")
-    @field:Min(value = 1, message = "설문조사 답변은 최소 1개이상 필요합니다.")
+    @field:Size(min = 1, message = "설문조사 답변은 최소 1개이상 필요합니다.")
     val answers: List<CreateSurveyResponseAnswerRequest>
 )
