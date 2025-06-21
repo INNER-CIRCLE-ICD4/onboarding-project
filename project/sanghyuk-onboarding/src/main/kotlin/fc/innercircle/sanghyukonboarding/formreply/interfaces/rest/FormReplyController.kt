55d0891c2b0971f5937fa422534601987c0d2efc
leesh5000 <leesh5000@gmail.com>
2025-06-21 09:12:31 +0900

    feature: 설문 조사 전체 요약 조회 API

--- project/sanghyuk-onboarding/src/main/kotlin/fc/innercircle/sanghyukonboarding/formreply/interfaces/rest/FormReplyController.kt
+++ project/sanghyuk-onboarding/src/main/kotlin/fc/innercircle/sanghyukonboarding/formreply/interfaces/rest/FormReplyController.kt
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
@@ -40,17 +39,6 @@ class FormReplyController(
         @PathVariable formId: String,
     ): ResponseEntity<ReplySummaryResponse> {
         val response: ReplySummaryResponse = summarizeFormRepliesUseCase.summarize(formId)
-        return ResponseEntity.ok(response)
-    }
-
-    @GetMapping("/replies", produces = ["application/json"])
-    fun getFormReplies(
-        @PathVariable formId: String,
-        @RequestParam page: Int = 0,
-        @RequestParam size: Int = 10,
-        @RequestParam questionName: String = ""
-    ): ResponseEntity<List<ReplySummaryResponse>> {
-        val response: List<ReplySummaryResponse> = summarizeFormRepliesUseCase.summarize(formId).replies
         return ResponseEntity.ok(response)
     }
 
