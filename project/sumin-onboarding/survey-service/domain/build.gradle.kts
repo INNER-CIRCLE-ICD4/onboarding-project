17cc2782d66a7c3aa2fddb9c9ab2ff943a006059
zoe <suumin0722@naver.com>
2025-06-23 16:56:44 +0900

    빌드 오류 해결2

--- project/sumin-onboarding/survey-service/domain/build.gradle.kts
+++ project/sumin-onboarding/survey-service/domain/build.gradle.kts
@@ -14,3 +14,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
