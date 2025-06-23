1a09a3b951f0227cb83de00ee82a39b0010b267c
zoe <suumin0722@naver.com>
2025-06-23 11:50:43 +0900

    lombok라이브러리 버전 명시

--- project/sumin-onboarding/survey-service/common/build.gradle.kts
+++ project/sumin-onboarding/survey-service/common/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.1.5")
 
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
