8eddebac2b09bcec78192f38f975e7def368a8b7
zoe <suumin0722@naver.com>
2025-06-23 13:04:17 +0900

    lombok라이브러리 버전 명시

--- project/sumin-onboarding/survey-service/domain/build.gradle.kts
+++ project/sumin-onboarding/survey-service/domain/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     implementation(project(":common"))
 
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.5")
 
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
