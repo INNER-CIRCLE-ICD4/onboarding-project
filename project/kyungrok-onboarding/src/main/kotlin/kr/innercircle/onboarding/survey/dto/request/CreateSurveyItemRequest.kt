34464224e4bb5ae1ade6f371b34f57acf3539026
ckr <ckr3453@gmail.com>
2025-07-14 20:05:12 +0900

    feat: 옵션 없이 request 요청 가능하게끔 수

--- project/kyungrok-onboarding/src/main/kotlin/kr/innercircle/onboarding/survey/dto/request/CreateSurveyItemRequest.kt
+++ project/kyungrok-onboarding/src/main/kotlin/kr/innercircle/onboarding/survey/dto/request/CreateSurveyItemRequest.kt
@@ -1,10 +1,12 @@
 package kr.innercircle.onboarding.survey.dto.request
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import kr.innercircle.onboarding.survey.domain.SurveyItemInputType
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class CreateSurveyItemRequest(
     @field:NotBlank(message = "설문조사 항목명은 필수값 입니다.")
     val name: String,
@@ -14,8 +16,7 @@ data class CreateSurveyItemRequest(
     val inputType: SurveyItemInputType,
 
     @field:Valid
-    @field:NotNull(message = "설문조사 항목 옵션 묶음은 필수값 입니다.")
-    val options: List<CreateSurveyItemOptionRequest>,
+    val options: List<CreateSurveyItemOptionRequest>?,
 
     @field:NotNull(message = "설문조사 항목 필수 여부는 필수값 입니다.")
     val isRequired: Boolean
